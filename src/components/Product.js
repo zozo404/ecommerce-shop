8635cd9e50b5317c2fcbbc2d08662bde277f1f24
zozo404 <enzocuny66@gmail.com>
2023-02-07 16:06:35 +0100

    🎨 | product card

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 // import link
 import { Link } from 'react-router-dom';
 // import icons
-import { BsPlug, BsEyeFill } from 'react-icons/bs'
+import { BsPlus, BsEyeFill } from 'react-icons/bs'
 
 const Product = ({ product }) => {
   console.log(product);
@@ -14,11 +14,29 @@ const Product = ({ product }) => {
         <div className='w-full h-full flex justify-center items-center'>
           {/* image */}
           <div className='w-[200px] mx-auto flex justify-center items-center'>
-            <img className='max-h-[160px] group-hover:scale-110' src={image} alt=''/>
+            <img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={image} alt='' />
           </div>
         </div>
+        {/* button */}
+        <div className='absolute top-6 -right-11 group-hover:right-4 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
+          <button>
+            <div className='flex justify-center items-center text-white w-12 h-12 bg-cyan-600'>
+              <BsPlus className="text-3xl" />
+            </div>
+          </button>
+          <Link to={`/product/${id}`} className='w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl'>
+            <BsEyeFill />
+          </Link>
+        </div>
       </div>
-      <div>2</div>
+      {/* category & title */}
+      <div>
+        <div className='text-sm capitalize text-gray-500 mb-1'>{category}</div>
+        <Link to={`/product/${id}`}>
+          <h2 className='font-semibold mb-1'>{title}</h2>
+        </Link>
+        <div className='font-semibold'>{price} €</div>
+      </div>
     </div>
   );
 };
