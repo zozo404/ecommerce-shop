00e6fe825f6cab0e97ebc43f25c09d3eeafb056a
zozo404 <enzocuny66@gmail.com>
2023-02-07 16:37:57 +0100

    ✨ | sidebarcontext

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,18 @@
 import React from 'react';
+// import link
+import { Link } from 'react-router-dom';
+// import icons
+import { IoMdArrowForward } from 'react-icons/io'
+import { FiTrash2 } from 'react-icons/fi'
+
+// import components
+import CartItem from './CartItem';
+// import sidebar context
+import SidebarContext from '../contexts/SidebarContext';
+import { useContext } from 'react';
 
 const Sidebar = () => {
+  const { isOpen, handleClose } = useContext(SidebarContext);
   return <div>Sidebar</div>;
 };
 
--- src/contexts/SidebarContext.js
+++ src/contexts/SidebarContext.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { createContext, useContext } from 'react';
+import { useState } from 'react';
 
-const SidebarContext = () => {
-  return <div>SidebarContext</div>;
+// create context
+export const SidebarContext = createContext()
+const SidebarProvider = ({children}) => {
+  // sidebar state
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  }
+
+  return <SidebarContext.Provider value={{isOpen, setIsOpen, handleClose}}>
+    {children}
+  </SidebarContext.Provider>;
 };
 
 export default SidebarContext;
