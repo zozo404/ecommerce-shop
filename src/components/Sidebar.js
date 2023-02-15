5a78af19f1a360b7c5857e0ad1876c91079c2391
zozo404 <enzocuny66@gmail.com>
2023-02-15 17:31:21 +0100

    🔧 sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,7 +13,7 @@ import { useContext } from 'react';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  return <div>Sidebar</div>;
+  return <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-al duration-300 z-20 px-4 lg:px-[35px]`}>Sidebar</div>;
 };
 
 export default Sidebar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,19 +13,19 @@ const Home = () => {
       item.category === "men's clothing" || item.category === "women's clothing"
     );
   });
-  return <div>
-    <section className='py-16'>
-      <div className='container mx-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
-          {filteredProducts.map(product => {
-            return (
-              <Product product={product} key={product.id} />
-            );
-          })}
+  return (
+    <div>
+      <section className='py-16'>
+        <div className='container mx-auto'>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
+            {filteredProducts.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
         </div>
-      </div>
-    </section>
-  </div>;
+      </section>
+    </div>
+  );
 };
 
 export default Home;
